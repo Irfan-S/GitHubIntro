c01ca1d966739ac6c9d375d517dffd22fdce4f8f
call-me-bk <karthik.bk2000@live.com>
2019-01-27 19:34:11 +0530

    //irfan you dumbass

--- app/src/main/java/oncreate/projects/githubintro/MainActivity.java
+++ app/src/main/java/oncreate/projects/githubintro/MainActivity.java
@@ -5,16 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {//irfan you suck
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {// irfan noob fuck
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    //Sent from alt account, check
+    }//genius lmao
+    //Sent from alt account, check //lame guy
     
     public void showToast(View v){
         Toast.makeText(this,"Test 2",Toast.LENGTH_LONG).show();
     }
-}
+}//fuck you
